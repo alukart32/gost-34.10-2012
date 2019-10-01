6230fd45705a32d2776c06acc2cb68457136ab94
Ivan_Tarasov <alukart32@gmail.com>
2019-10-01 09:53:50 +0300

    Done. Need to add form for gost's tests

---
+++ GostECC/DigitalSign.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigitalSignECC
+{
+    // класс параметров схемы цифровой подписи
+    class DigitalSign
+    {
+        private BigInteger p = new BigInteger();
+        private BigInteger a = new BigInteger();
+        private BigInteger b = new BigInteger();
+        // порядок подгруппы группы точек эллиптической кривой;
+        private BigInteger q = new BigInteger();
+
+        private byte[] xG;
+        // точка эллептической кривой
+        private ECPoint Q = new ECPoint();
+        
+        public DigitalSign(BigInteger p, BigInteger a, BigInteger b, BigInteger n, byte[] xG)
+        {
+            this.a = a;
+            this.b = b;
+            this.q = n;
+            this.p = p;
+            this.xG = xG;
+        }
+
+        //Генерируем секретный ключ заданной длины
+        public BigInteger GenereatePrivateKey(int BitSize)
+        {
+            BigInteger d = new BigInteger();
+
+            do
+            {
+                d.genRandomBits(BitSize, new Random());
+            } while ((d < 0) || (d > q));
+
+            return d;
+        }
+
+        //С помощью секретного ключа d вычисляем точку Q=d*G, это и будет наш публичный ключ
+        public ECPoint GeneratePublicKey(BigInteger d)
+        {
+            ECPoint Q = ECPoint.multiply(d, QDecompression());
+            return Q;
+        }
+
+        //Восстанавливаем координату y из координаты x и бита четности y 
+        private ECPoint QDecompression()
+        {
+            byte y = xG[0];
+            byte[] x = new byte[xG.Length-1];
+
+            Array.Copy(xG, 1, x, 0, xG.Length - 1);
+
+            BigInteger Xcord = new BigInteger(x);
+
+            // y^2 = x^3 + a*x+b - эллип. уравнение
+            BigInteger temp = (Xcord * Xcord * Xcord + a * Xcord + b) % p;
+
+            BigInteger beta = ModSqrt(temp, p);
+            BigInteger Ycord = new BigInteger();
+
+            if ((beta % 2) == (y % 2))
+                Ycord = beta;
+            else
+                Ycord = p - beta;
+
+            ECPoint Q = new ECPoint();
+
+            Q.a = a;
+            Q.b = b;
+            Q.FieldChar = p;
+            Q.x = Xcord;
+            Q.y = Ycord;
+
+            this.Q = Q;
+
+            return Q;
+        }
+
+        //функция вычисления квадратоного корня по модулю простого числа q
+        public BigInteger ModSqrt(BigInteger a, BigInteger q)
+        {
+            BigInteger b = new BigInteger();
+
+            do
+            {
+                b.genRandomBits(255, new Random());
+            } while (Legendre(b, q) == 1);
+
+            BigInteger s = 0;
+            BigInteger t = q - 1;
+
+            while ((t & 1) != 1)
+            {
+                s++;
+                t = t >> 1;
+            }
+
+            BigInteger InvA = a.modInverse(q);
+            BigInteger c = b.modPow(t, q);
+            BigInteger r = a.modPow(((t + 1) / 2), q);
+            BigInteger d = new BigInteger();
+
+            for (int i = 1; i < s; i++)
+            {
+                BigInteger temp = 2;
+                temp = temp.modPow((s - i - 1), q);
+
+                d = (r.modPow(2, q) * InvA).modPow(temp, q);
+
+                if (d == (q - 1))
+                    r = (r * c) % q;
+                c = c.modPow(2, q);
+            }
+
+            return r;
+        }
+
+        //вычисляем символ Лежандра
+        public BigInteger Legendre(BigInteger a, BigInteger q)
+        {
+            return a.modPow((q - 1) / 2, q);
+        }
+
+        //подписываем сообщение
+        // msg - сообщение
+        // d - ключ подписи
+        public string SingMsg(byte[] msg, BigInteger d)
+        {
+            // Шаг 2: вычислить e
+            BigInteger e = new BigInteger(msg) % q;
+
+            if (e == 0)
+                e = 1;
+
+            BigInteger k = new BigInteger();
+            // точка эллиптической кривой
+            ECPoint C = new ECPoint();
+
+            BigInteger r = new BigInteger();
+            BigInteger s = new BigInteger();
+
+            do
+            {
+                // Шаг 3: сгенерировать случайное k (0< k <q)
+                do
+                {
+                    k.genRandomBits(q.bitCount(), new Random());
+                } while ((k < 0) || (k > q));
+
+                // найти точку элл. C = kP
+                C = ECPoint.multiply(k, Q);
+                // и определить r = Xc(mod q)
+                r = C.x % q;
+                // определим s = (rd + ke)(mod q), s == 0 -> Шаг 3
+                s = ((r * d) + (k * e)) % q;
+
+            } while ((r == 0)||(s==0));
+
+            // перевод значений в двоичный вид
+            string Rvector = padding(r.ToHexString(),q.bitCount()/4);
+
+            string Svector = padding(s.ToHexString(), q.bitCount() / 4);
+
+            // конкатенацию двух двоичных векторов  (цифровая подпись)
+            return Rvector + Svector;
+        }
+
+        //проверяем подпись 
+        // H - хэш-сумма
+        // sing - подпись
+        // Q - ключ проверки подписи
+        public bool SingVer(byte[] H, string sing, ECPoint Q)
+        {
+            // Шаг 1: вычисляем целые числа r и s по полученной подписи
+            string Rvector = sing.Substring(0, q.bitCount() / 4);
+            string Svector = sing.Substring(q.bitCount() / 4, q.bitCount() / 4);
+
+            BigInteger r = new BigInteger(Rvector, 16);
+            BigInteger s = new BigInteger(Svector, 16);
+
+            if ((r < 1) || (r > (q - 1)) || (s < 1) || (s > (q - 1)))
+                return false;
+
+            // Шаг 3    
+            BigInteger alpha = new BigInteger(H);
+            BigInteger e = alpha % q;
+
+            if (e == 0)
+                e = 1;
+            
+            // Шаг 4
+            BigInteger v = e.modInverse(q);
+
+            // Шаг 5
+            BigInteger z1 = (s * v) % q;
+            BigInteger z2 = q + ((-(r * v)) % q);
+
+            // восст. y по x
+            this.Q = QDecompression();
+
+            // Шаг 6: найти C = z1 P + z2 Q
+            ECPoint A = ECPoint.multiply(z1, this.Q);
+            ECPoint B = ECPoint.multiply(z2, Q);
+            ECPoint C = A + B;
+            // R = xc (mod q)
+            BigInteger R = C.x % q;
+
+            //Шаг 7
+            if (R == r)
+                return true;
+            else
+                return false;
+        }
+
+        //дополняем подпись нулями слева до длины n, где n - длина модуля в битах
+        private string padding(string input, int size)
+        {
+            if (input.Length < size)
+            {
+                do
+                {
+                    input = "0" + input;
+                } while (input.Length < size);
+            }
+            return input;
+        }
+    }
+}
---
+++ GostECC/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace DigitalSignECC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.signButton = new System.Windows.Forms.Button();
+            this.verifyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(9, 49);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(580, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(9, 124);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(580, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // signButton
+            // 
+            this.signButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.signButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.signButton.Location = new System.Drawing.Point(425, 182);
+            this.signButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.signButton.Name = "signButton";
+            this.signButton.Size = new System.Drawing.Size(171, 47);
+            this.signButton.TabIndex = 3;
+            this.signButton.Text = "Sign";
+            this.signButton.UseVisualStyleBackColor = true;
+            this.signButton.Click += new System.EventHandler(this.SignButton_Click);
+            // 
+            // verifyButton
+            // 
+            this.verifyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.verifyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.verifyButton.Location = new System.Drawing.Point(425, 250);
+            this.verifyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.verifyButton.Name = "verifyButton";
+            this.verifyButton.Size = new System.Drawing.Size(171, 45);
+            this.verifyButton.TabIndex = 4;
+            this.verifyButton.Text = "Verify";
+            this.verifyButton.UseVisualStyleBackColor = true;
+            this.verifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Msg";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Signed Msg";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 8;
+            // 
+            // bTest
+            // 
+            this.bTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bTest.Location = new System.Drawing.Point(9, 250);
+            this.bTest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bTest.Name = "bTest";
+            this.bTest.Size = new System.Drawing.Size(112, 39);
+            this.bTest.TabIndex = 9;
+            this.bTest.Text = "Tests";
+            this.bTest.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 322);
+            this.Controls.Add(this.bTest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.verifyButton);
+            this.Controls.Add(this.signButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button signButton;
+        private System.Windows.Forms.Button verifyButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bTest;
+    }
+}
+
